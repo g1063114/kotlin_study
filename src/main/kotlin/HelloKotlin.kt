a5a849525144ec146d70b60445e6b64009ca2429
jinho <g1063114@naver.com>
2021-12-11 23:18:35 +0900

    for loop test

--- src/main/kotlin/HelloKotlin.kt
+++ src/main/kotlin/HelloKotlin.kt
@@ -8,9 +8,14 @@ fun welcome(msg : String) : Unit{
 fun main(args: Array<String>){
     var numbers = arrayOf(1,4,3,6,8,9);
     var sum = 0;
+    var num = 0;
 
     for(n in numbers){
         sum+=n;
     }
-    print("$sum");
+
+    for(i in 1..10){
+        num+=i;
+    }
+    print("$num");
 }
